040748046c15cb9f46070c58eec4d97dc32d78f4
ragnar-pwninskjold <seankochel@gmail.com>
2016-06-18 16:22:50 -0400

    fixed sound

--- js/app.js
+++ js/app.js
@@ -1,38 +1,35 @@
+
+
 $(document).ready(function(){
-
-	$(".ryu").mouseenter(function() {
-
-		$(".ryustill").hide();
-		$(".ryuready").show();
-	})
-	.mouseleave(function(){
-
-		$(".ryuready").hide();
-		$(".ryustill").show();
-
-	})
-	.mousedown(function() {
-		playHadouken();
-		$(".ryuready").hide();
-		$(".ryuhad").show();
-		$(".hadouken").finish().show().animate(
-			{"left": "1020px"}, 
-			500,
-			function(){
-				$(this).hide();
-				$(this).css("left", "520px");
-			});
-		
-	})
-	.mouseup(function() {
-		$(".ryuhad").hide();
-		$(".ryuready").show();
-	});
+    $('.ryu').mouseenter(function(){
+        $('.ryustill').hide();
+        $('.ryuready').show();
+    })
+    .mouseleave(function() {
+        $('.ryustill').show();
+        $('.ryuready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryuready').hide();
+        $('.ryuhad').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryuhad').hide();
+        $('.ryuready').show();
+    });
 
 });
 
-function playHadouken () {
-	$("#hadouken-sound")[0].volume = 0.5;
-	$("#hadouken-sound")[0].load();
-	$("#hadokuen-sound")[0].play();
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
 }
